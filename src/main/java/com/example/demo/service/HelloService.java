33e48f51efc93615bdad65847745e74262e0a087
shaikismail595 <ismailshaik595@gmail.com>
2025-02-12 12:10:00 +0530

    Added Import ot fix compilation issue

--- src/main/java/com/example/demo/service/HelloService.java
+++ src/main/java/com/example/demo/service/HelloService.java
@@ -1,5 +1,6 @@
 package com.example.demo.service;
 
+import com.example.demo.repository.HelloRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
